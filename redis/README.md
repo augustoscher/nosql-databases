1b35293b854dadc28c654f7c7f32e1774a6b12b7
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-29 13:29:30 -0300

    Redinsgo commands

--- redis/README.md
+++ redis/README.md
@@ -6,12 +6,13 @@ Notes:
 #### Setup
 1. Running Redis container:
    > docker run -d -p 6379:6379 --name myredis redis
-   
+
 2. Clone repository:
    > git clone git@github.com:augustoscher/nosql-databases.git
 
 3. Install dependencies:
-   > cd nosql-databases && cd redis && npm install
+   > cd nosql-databases && cd redis
+   > npm install
 
 4. Run
    > npm run redinsgo
--- redis/redinsgo.js
+++ redis/redinsgo.js
@@ -1,6 +1,43 @@
+const redis = require("redis");
+const USERS_KEY = 'users';
+
+const getUsers = () => {
+  let users = [];
+  for (let i = 0; i < 50; i++) {
+    users.push(`user:0${i+1}`);
+  }
+  return users;
+}
 
 const main = () => {
   console.log("Hello Redinsgo...")
+  const client = redis.createClient();
+
+  client.on("error", error => {
+    console.error(error);
+  });
+
+  client.on('connect', () => {
+    console.log('connected');
+  });
+
+  // const users = getUsers();
+  // client.hmset(USERS_KEY, users);
+
+  client.hmset('users', 'portugues', 'ingles', 'espanhol', 'alemao');
+  client.hgetall(USERS_KEY, function (err, object) {
+    console.log(object);
+  });
+
+  console.log();
+  client.sadd(['tags', 'angularjs', 'angularjs', 'backbonejs', 'emberjs'], function (err, reply) {
+    console.log(reply);
+  });
+
+  client.smembers('tags', function (err, reply) {
+    console.log(reply);
+  });
+
 }
 
 main();
